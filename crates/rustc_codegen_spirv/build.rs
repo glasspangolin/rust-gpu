e8574448242ea56a586948949c38f1892105133c
glasspangolin <23103433+glasspangolin@users.noreply.github.com>
2024-11-18 15:25:30 +1100

    Revert "Two days later"
    This reverts commit dea13add0439f82b08cfc7808c84f0d20dbc18e4.

--- crates/rustc_codegen_spirv/build.rs
+++ crates/rustc_codegen_spirv/build.rs
@@ -10,8 +10,9 @@ use std::process::{Command, ExitCode};
 /// `cargo publish`. We need to figure out a way to do this properly, but let's hardcode it for now :/
 //const REQUIRED_RUST_TOOLCHAIN: &str = include_str!("../../rust-toolchain.toml");
 const REQUIRED_RUST_TOOLCHAIN: &str = r#"[toolchain]
-channel = "nightly-2024-04-26"
-components = ["rust-src", "rustc-dev", "llvm-tools"]"#;
+channel = "nightly-2024-04-24"
+components = ["rust-src", "rustc-dev", "llvm-tools"]
+# commit_hash = 244da22fabd9fa677bbd0ac601a88e5ca6917526"#;
 
 fn get_rustc_commit_hash() -> Result<String, Box<dyn Error>> {
     let rustc = std::env::var("RUSTC").unwrap_or_else(|_| String::from("rustc"));
